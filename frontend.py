1066ef9e00bd4b30cc2f3cd6906d5e1f35995981
borisconstantin <54483884+borisconstantin@users.noreply.github.com>
2025-04-01 04:03:20 +0000

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -58,8 +58,7 @@ def main():
 
             for site, link in zip(recommendations, links):
                 st.image(f'Donnees/sites logo/{site.lower().split('.')[0]}.png')
-                st.write(link)
-                st.link_button("Visit", link)
+                st.link_button("Visit", link[0])
 
 
 
