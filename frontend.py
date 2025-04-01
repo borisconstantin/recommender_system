f0d94fb90f69d8f957bb9dbb6245d1cca96a3137
borisconstantin <54483884+borisconstantin@users.noreply.github.com>
2025-04-01 03:58:12 +0000

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -57,6 +57,7 @@ def main():
                                             user_need_discount)
 
             for site, link in zip(recommendations, links):
+                st.write(site)
                 st.image(f'Donnees/sites logo/{site.lower()}.png')
                 st.success(link)
 
