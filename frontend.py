0bd6f5132f12d99a2696b7c188c23e9c7097f9ad
borisconstantin <54483884+borisconstantin@users.noreply.github.com>
2025-04-01 04:04:39 +0000

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -57,8 +57,9 @@ def main():
                                             user_need_discount)
 
             for site, link in zip(recommendations, links):
-                st.image(f'Donnees/sites logo/{site.lower().split('.')[0]}.png')
-                st.link_button("Visit", link[0])
+                site = site.split('.')[0]
+                st.image(f'Donnees/sites logo/{site.lower()[0]}.png')
+                st.link_button(f"Visit {site}", link[0])
 
 
 
